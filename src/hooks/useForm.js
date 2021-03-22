9ad33279deeed4e78a46466e62b0245fcddd3cbe
unknown <allenmk20073@yahoo.com>
2021-03-21 21:02:46 -0400

    App finished. onward to testing

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,1 +1,20 @@
 // write your custom hook here to control your checkout form
+import {useState} from 'react'
+
+function useForm (props) {
+    const [showSuccessMessage, setSuccessMessage] = useState(false)
+    const [values, setValues] = useState(props)
+
+    const handleChanges = (e) => {
+        setValues({ ...values, [e.target.name] : e.target.value })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        setSuccessMessage(true)
+    }
+
+    return [showSuccessMessage, values, handleChanges, handleSubmit]
+}
+
+export default useForm
